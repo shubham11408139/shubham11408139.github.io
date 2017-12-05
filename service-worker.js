80f687c7bb8e258e4a0b0063d6e88de8943fe3f9
shubham11408139 <shubhamgupta.608@rediffmail.com>
2017-12-06 02:19:29 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -58,6 +58,8 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+//for Notification
 self.addEventListener('notificationclick',function(e) {
   var notification = e.notification;
   var primaryKey = notification.data.primaryKey;
